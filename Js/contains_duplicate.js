1a40149dff501a9613bd76575d9e9461b274cee6
Protik111 <rafiurprotik111@gmail.com>
2022-02-07 09:41:57 +0600

    time complexity O(n) added for contains duplicate

--- Js/contains_duplicate.js
+++ Js/contains_duplicate.js
@@ -11,6 +11,21 @@ const containDuplicates = (nums) => {
     }
     return false;
 }
-
 const nums = [1, 3, 5];
 console.log(containDuplicates(nums));
+
+//time complexity O(n)
+var containsDuplicate = function (nums2) {
+    // const map = {}
+    // for (let i of nums) {
+    //     if (map[i]) return true
+    //     map[i] = true
+    // }
+    // return false
+
+    const set = new Set(nums2);
+    return set.size !== nums2.length
+};
+
+const nums2 = [1, 3, 5, 5];
+console.log(containsDuplicate(nums2));
