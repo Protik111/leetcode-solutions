5a78f49fdeaff12b14773f2db7efa39025d0418a
Protik111 <rafiurprotik111@gmail.com>
2022-02-19 22:12:50 +0600

    longest common predix added

---
+++ Js/longest_common_prefix.js
@@ -0,0 +1,15 @@
+const longestCommonPrefix = (strs) => {
+    if (!strs.length) return '';
+
+    for (let i = 0; i <= strs[0].length; i++) {
+        for (let j = 1; j < strs.length; j++) {
+            if (strs[0][i] !== strs[j][i]) {
+                return strs[0].slice(0, i);
+            }
+        }
+    }
+
+    return strs[0];
+};
+
+console.log(longestCommonPrefix(["dog","racecar","car"]));
