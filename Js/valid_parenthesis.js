e8224946d7db8e5fff1eb94db6486f1b02dfd229
Protik111 <rafiurprotik111@gmail.com>
2022-03-01 11:08:37 +0600

    valid parenthesis with js added

---
+++ Js/valid_parenthesis.js
@@ -0,0 +1,19 @@
+const isValid = (paren) => {
+    if(paren.length % 2 !== 0) return false;
+    const parenPair = new Map([
+        ['(' , ')'],
+        ['{' , '}'],
+        ['[' , ']'],
+    ]);
+    const stack = [];
+    for(let i of paren){
+        if(parenPair.has(i)){
+            stack.push(parenPair.get(i));
+        }else if(i !== stack.pop()){
+            return false;
+        }
+    }
+    return stack.length === 0;
+}
+
+console.log(isValid('(]'));
